30d1da07af7d01a6784ee5db28aef9fd71a4bd0d
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-06-13 12:53:52 +0700

    updateRoomlist

--- src/components/ui/CalenderComponent/CalenderComponent.jsx
+++ src/components/ui/CalenderComponent/CalenderComponent.jsx
@@ -80,7 +80,7 @@ export const CalenderComponent = ({ chiTietPhong, maPhong }) => {
         const paymentResponse = await axios.post("https://serverpayment.vercel.app/payment", {
           amount: chiTietPhong?.giaTien * 2300,
           orderInfo: "Thanh toán đặt phòng",
-          redirectUrl: "",
+          redirectUrl: `${window.location.origin}/payment-confirmation`,
           ipnUrl: 'https://webhook.site/5254fac2-369f-4f25-b13b-0ad3a1f1e5e0'
         });
         
