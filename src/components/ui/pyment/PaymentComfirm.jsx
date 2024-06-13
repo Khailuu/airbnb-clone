4d665a0a110155b4be3d4865f288c1028c84bc74
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-06-13 11:25:14 +0700

    fixPayment

--- src/components/ui/pyment/PaymentComfirm.jsx
+++ src/components/ui/pyment/PaymentComfirm.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { usePostDatPhong } from "../../../hooks/api/quanLyDatPhongApi/usePostDatPhong";
 import queryString from 'query-string';
+import { PATH } from '../../../constant';
 
 export const PaymentComfirm = () => {
     const location = useLocation();
@@ -18,7 +19,7 @@ export const PaymentComfirm = () => {
       mutation.mutate(bookingData, {
         onSuccess: () => {
           toast.success("Đặt phòng thành công!");
-          navigate(PATH.DASHBOARD); // chuyển hướng đến bảng điều khiển hoặc trang khác
+          navigate(PATH.payment); // chuyển hướng đến bảng điều khiển hoặc trang khác
         },
         onError: (error) => {
           toast.error("Đặt phòng thất bại!");
