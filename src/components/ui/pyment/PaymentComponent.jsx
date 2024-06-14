e25ca6c3bb43bec7e428f6ef0479252ee15be4f7
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-06-14 08:46:21 +0700

    update paymentcomponent

--- src/components/ui/pyment/PaymentComponent.jsx
+++ src/components/ui/pyment/PaymentComponent.jsx
@@ -5,7 +5,7 @@ import { PATH } from '../../../constant'
 export const PaymentComponent = () => {
   return (
     <div className='flex flex-col items-center justify-center my-[40px]'>
-      <img className='w-[50%]' src="../../../../public/images/payment-success.jpg" alt="image success" />
+      <img className='w-[50%]' src="../../../../images/payment-success.jpg" alt="image success" />
       <h2 className='mb-[20px] text-rose-500 text-[40px]'>Đơn hàng của bạn đã được thanh toán thành công!</h2>
       <NavLink to={PATH.home} style={{ textDecoration: "underline" }} className="hover:text-rose-500 transition-all ease-in-out">Quay lại trang chủ</NavLink>
     </div>
