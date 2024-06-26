7a9d50146e9004f2391cead79ecfb6c99c5afb95
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-06-26 16:56:02 +0700

    UpdateLanguage

--- src/components/ui/User/EditProfile.jsx
+++ src/components/ui/User/EditProfile.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { quanLyUserServices } from '../../../services/QuanLyUser';
-import { useFormik } from 'formik';
-import { Button, DatePicker, Form, Input, Radio } from 'antd';
-import { LOCAL_USER_LOGIN_KEY } from '../../../constant';
-import { useEditNguoiDung } from '../../../hooks/api/quanLyNguoiDungApi/useEditNguoiDung';
-import { toast } from 'react-toastify';
-import { useDispatch, useSelector } from 'react-redux';
-import { quanLyNguoiDungAction } from '../../../store/quanLyNguoiDung/slice';
-import dayjs from 'dayjs';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { quanLyUserServices } from "../../../services/QuanLyUser";
+import { useFormik } from "formik";
+import { Button, DatePicker, Form, Input, Radio } from "antd";
+import { LOCAL_USER_LOGIN_KEY } from "../../../constant";
+import { useEditNguoiDung } from "../../../hooks/api/quanLyNguoiDungApi/useEditNguoiDung";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { quanLyNguoiDungAction } from "../../../store/quanLyNguoiDung/slice";
+import dayjs from "dayjs";
 
 export const EditProfile = () => {
   const { id } = useParams();
@@ -19,6 +19,7 @@ export const EditProfile = () => {
   const userLogin = useSelector((state) => state.quanLyNguoiDung);
   const [componentSize, setComponentSize] = useState("default");
   const [disable, setDisable] = useState(true);
+  const [disableRole, setDisableRole] = useState(true);
   const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
@@ -31,36 +32,41 @@ export const EditProfile = () => {
     enableReinitialize: true,
     initialValues: {
       id: user?.id || 0,
-      name: user?.name || '',
-      email: user?.email || '',
-      phone: user?.phone || '',
-      birthday: user?.birthday || '',
+      name: user?.name || "",
+      email: user?.email || "",
+      phone: user?.phone || "",
+      birthday: user?.birthday || "",
       gender: user?.gender || true,
-      role: user?.role || '',
+      role: user?.role || "",
       avatar: user?.avatar || "",
       password: user?.password || "",
     },
-    onSubmit: values => {
-      const token = userLogin.userLogin.token
-      mutation.mutate({ id: user?.id, payload: values }, {
-        onSuccess: () => {
-          const profile = {
-            token,
-            user: values
-          }
-          dispatch(quanLyNguoiDungAction.updateUserLogin({
-            ...profile
-          }));
-          localStorage.setItem(LOCAL_USER_LOGIN_KEY, JSON.stringify(profile))
-          toast.success("Cập nhật thông tin thành công!");
-          setIsEditing(false); 
-          setDisable(true);
-        },
-        onError: (err) => {
-          toast.error(err);
+    onSubmit: (values) => {
+      const token = userLogin.userLogin.token;
+      mutation.mutate(
+        { id: user?.id, payload: values },
+        {
+          onSuccess: () => {
+            const profile = {
+              token,
+              user: values,
+            };
+            dispatch(
+              quanLyNguoiDungAction.updateUserLogin({
+                ...profile,
+              })
+            );
+            localStorage.setItem(LOCAL_USER_LOGIN_KEY, JSON.stringify(profile));
+            toast.success("Cập nhật thông tin thành công!");
+            setIsEditing(false);
+            setDisable(true);
+          },
+          onError: (err) => {
+            toast.error(err);
+          },
         }
-      });
-    }
+      );
+    },
   });
 
   const onFormLayoutChange = ({ size }) => {
@@ -69,6 +75,12 @@ export const EditProfile = () => {
 
   const handleBtnChinhSua = () => {
     setDisable(false);
+    if (formik.values.role === "ADMIN") {
+      setDisableRole(false);
+    }
+    else if (formik.values.role === "USER") {
+      setDisableRole(true);
+    }
     setIsEditing(true);
   };
   const handleChangeDatePicker = (date) => {
@@ -80,9 +92,9 @@ export const EditProfile = () => {
   };
 
   return (
-    <div className='container mx-auto my-[40px]'>
+    <div className="container mx-auto my-[40px]">
       <h2 className="text-rose-500 text-[25px] font-bold mb-[30px]">
-        Cập nhật thông tin: 
+        Cập nhật thông tin:
       </h2>
       <Form
         labelCol={{ span: 4 }}
@@ -102,48 +114,82 @@ export const EditProfile = () => {
           </Radio.Group>
         </Form.Item>
         <Form.Item label="Id">
-          <Input name="id" disabled value={formik.values.id} onChange={formik.handleChange} />
+          <Input
+            name="id"
+            disabled
+            value={formik.values.id}
+            onChange={formik.handleChange}
+          />
         </Form.Item>
         <Form.Item label="Name">
-          <Input name="name" disabled={disable} value={formik.values.name} onChange={formik.handleChange} />
+          <Input
+            name="name"
+            disabled={disable}
+            value={formik.values.name}
+            onChange={formik.handleChange}
+          />
         </Form.Item>
         <Form.Item label="Email">
-          <Input name="email" disabled={disable} value={formik.values.email} onChange={formik.handleChange} />
+          <Input
+            name="email"
+            disabled={disable}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
         </Form.Item>
         <Form.Item label="Phone">
-          <Input name="phone" disabled={disable} value={formik.values.phone} onChange={formik.handleChange} />
+          <Input
+            name="phone"
+            disabled={disable}
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+          />
         </Form.Item>
         <Form.Item label="Birthday">
-          <DatePicker 
+          <DatePicker
             disabled={disable}
-            name="birthday" 
-            value={dayjs(formik.values.birthday ,"DD/MM/YYYY")} 
-            format='DD-MM-YYYY'
-            onChange={handleChangeDatePicker} 
+            name="birthday"
+            value={dayjs(formik.values.birthday, "DD/MM/YYYY")}
+            format="DD-MM-YYYY"
+            onChange={handleChangeDatePicker}
           />
         </Form.Item>
         <Form.Item label="Gender">
-          <Radio.Group disabled={disable} name="gender" value={formik.values.gender} onChange={formik.handleChange}>
+          <Radio.Group
+            disabled={disable}
+            name="gender"
+            value={formik.values.gender}
+            onChange={formik.handleChange}
+          >
             <Radio value={true}>Male</Radio>
             <Radio value={false}>Female</Radio>
           </Radio.Group>
         </Form.Item>
         <Form.Item label="Role">
-          <Input name="role" disabled={disable} value={formik.values.role.toUpperCase()} onChange={formik.handleChange} />
+          <Input
+            name="role"
+            disabled={disableRole}
+            value={formik.values.role.toUpperCase()}
+            onChange={formik.handleChange}
+          />
         </Form.Item>
         <Form.Item>
-          <Button 
-            className={`bg-rose-500 text-white rounded-[6px] ${!isEditing ? 'hidden' : ''}`} 
-            type="primary" 
+          <Button
+            className={`bg-rose-500 text-white rounded-[6px] ${
+              !isEditing ? "hidden" : ""
+            }`}
+            type="primary"
             htmlType="submit"
             onClick={() => setDisable(false)}
             loading={mutation.isPending}
           >
             Cập Nhật
           </Button>
-          <Button 
-            className={`bg-rose-500 text-white rounded-[6px] ${isEditing ? 'hidden' : ''}`} 
-            type="primary" 
+          <Button
+            className={`bg-rose-500 text-white rounded-[6px] ${
+              isEditing ? "hidden" : ""
+            }`}
+            type="primary"
             onClick={handleBtnChinhSua}
           >
             Chỉnh sửa
