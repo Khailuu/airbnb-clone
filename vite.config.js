40f7c6c7dff443aadbb619b871f7d8e92fcadd3c
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-07-08 20:49:35 +0700

    optimize

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'gzip',
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+  ],
   server: {
-    port: 3000
+    port: 3000,
   },
   optimizeDeps: {
-    exclude: ['query-string'],
+    exclude: ["query-string"],
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      assets: path.resolve(__dirname, './src/aasets'),
-      components: path.resolve(__dirname, './src/components'),
-      layouts: path.resolve(__dirname, './src/components/layouts'),
-      template: path.resolve(__dirname, './src/components/template'),
-      ui: path.resolve(__dirname, './src/components/ui'),
-      constant: path.resolve(__dirname, './src/constant'),
-      contexts: path.resolve(__dirname, './src/contexts'),
-      hooks: path.resolve(__dirname, './src/hooks'),
-      libs: path.resolve(__dirname, './src/libs'),
-      pages: path.resolve(__dirname, './src/pages'),
-      routers: path.resolve(__dirname, './src/routers'),
-      schemas: path.resolve(__dirname, './src/schemas'),
-      services: path.resolve(__dirname, './src/services'),
-      store: path.resolve(__dirname, './src/store'),
-      types: path.resolve(__dirname, './src/types'),
-      utils: path.resolve(__dirname, './src/utils'),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+      assets: path.resolve(__dirname, "./src/aasets"),
+      components: path.resolve(__dirname, "./src/components"),
+      layouts: path.resolve(__dirname, "./src/components/layouts"),
+      template: path.resolve(__dirname, "./src/components/template"),
+      ui: path.resolve(__dirname, "./src/components/ui"),
+      constant: path.resolve(__dirname, "./src/constant"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      libs: path.resolve(__dirname, "./src/libs"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      routers: path.resolve(__dirname, "./src/routers"),
+      schemas: path.resolve(__dirname, "./src/schemas"),
+      services: path.resolve(__dirname, "./src/services"),
+      store: path.resolve(__dirname, "./src/store"),
+      types: path.resolve(__dirname, "./src/types"),
+      utils: path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
