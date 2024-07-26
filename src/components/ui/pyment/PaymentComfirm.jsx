d1828bdb4e6c052d18bc8dec580de377ed5ca8e0
khailuu <khailuu123@gmail.com>
2024-07-27 00:51:51 +0700

    fix scroll top

--- src/components/ui/pyment/PaymentComfirm.jsx
+++ src/components/ui/pyment/PaymentComfirm.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { usePostDatPhong } from "../../../hooks/api/quanLyDatPhongApi/usePostDatPhong";
-import queryString from 'query-string';
-import { PATH } from '../../../constant';
-import { useTranslation } from 'react-i18next';
+import queryString from "query-string";
+import { PATH } from "../../../constant";
+import { useTranslation } from "react-i18next";
+import { Spin } from "antd";
 
 export const PaymentComfirm = () => {
   const navigate = useNavigate();
   const mutation = usePostDatPhong();
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   useEffect(() => {
-    const return_code = "1"
+    const return_code = "1";
     if (return_code) {
-      const bookingData = JSON.parse(localStorage.getItem('bookingData'));
+      const bookingData = JSON.parse(localStorage.getItem("bookingData"));
 
       if (bookingData) {
         mutation.mutate(bookingData, {
@@ -25,7 +26,7 @@ export const PaymentComfirm = () => {
           onError: (error) => {
             console.error("Mutation error:", error);
             toast.error("Đặt phòng thất bại!");
-          }
+          },
         });
       } else {
         console.error("No booking data found in localStorage.");
@@ -38,8 +39,18 @@ export const PaymentComfirm = () => {
   }, []);
 
   return (
-    <div className='flex items-center justify-center'>
-      <h2 className='text-rose-500 text-[40px]'>{t("Đang xử lý thanh toán...")}</h2>
+    <div className="flex items-center flex-col justify-center">
+      <h2 className="text-rose-500 text-[40px]">
+        {t("Đang xử lý thanh toán...")}
+      </h2>
+      <div className="text-[24px] text-rose-500">
+        <Spin
+          className="custom-spin"
+          tip="Loading"
+          size="large"
+          style={{ colorPrimary: "#f43f5e !important" }}
+        ></Spin>
+      </div>
     </div>
   );
 };
